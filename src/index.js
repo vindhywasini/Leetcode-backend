6e3059b317a608837d225a7b53d9909feaa683d3
Vindhywasini Gupta <gvindhywasini@gmail.com>
2025-08-15 13:26:13 +0530

    cors

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ const cors = require('cors')
 // console.log("Hello")
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://leetcode-frontend-ikts.onrender.com',
     credentials: true 
 }))
 
