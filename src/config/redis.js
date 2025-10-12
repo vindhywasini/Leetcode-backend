84ae8b07c977bf72c5be241f5df33456933b39e1
vindhywasini <112778149+vindhywasini@users.noreply.github.com>
2025-10-12 21:26:21 +0530

    Update redis.js

--- src/config/redis.js
+++ src/config/redis.js
@@ -4,8 +4,8 @@ const redisClient = createClient({
     username: 'default',
     password: process.env.REDIS_PASS,
     socket: {
-        host: 'redis-13805.crce179.ap-south-1-1.ec2.redns.redis-cloud.com',
-        port: 13805
+        host: 'redis-10674.c305.ap-south-1-1.ec2.redns.redis-cloud.com',
+        port: 10674
     }
 });
 
